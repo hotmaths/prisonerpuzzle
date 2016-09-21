6fcc1bf0598d83bb7e36cf4a615257171d704b8a
Conley Owens <cco3@inkylabs.com>
2016-09-20 19:51:28 -0700

    Use knowledge of other prisoners in CC strategy
    Instead of only turning on the light bulb on your day, also turn on the
    light bulb on days when you have seen the light bulb on.

--- strategies/collectiveconsciousness.py
+++ strategies/collectiveconsciousness.py
@@ -4,10 +4,10 @@ import prisonerlib
 class Prisoner(prisonerlib.Prisoner):
     def __init__(self, *args, **kwargs):
         super(Prisoner, self).__init__(*args, **kwargs)
-        self.__memory = set()
+        self.__memory = set([self.pid])
 
     def visit(self, light_bulb, day):
         if light_bulb.on:
             self.__memory.add((day - 1) % prisonerlib.NUM_PRISONERS)
-        light_bulb.on = self.pid == day % prisonerlib.NUM_PRISONERS
+        light_bulb.on = day % prisonerlib.NUM_PRISONERS in self.__memory
         return len(self.__memory) == prisonerlib.NUM_PRISONERS
